3e394f7648c831b8f0630d24073c79c9f6efe4ab
残念弹幕世界 <33081329+snowflowersnowflake@users.noreply.github.com>
2019-02-16 13:54:19 +0800

    Add files via upload

---
+++ 短学期答辩/python程序设计/re.py
@@ -0,0 +1,19 @@
+#re
+for i in range(5):
+    print(i)
+for c in "HELLO":
+    print(c,end=',')
+print()
+for item in [123,"py",456]:
+    print(item,end=',')
+print()
+
+a=3
+while a>0:
+    a=a-1
+    print(a)
+
+while a>0:
+    a=a+1
+    print(a)
+
---
+++ 短学期答辩/python程序设计/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+a = [1,2]
+b = [3,4]
+m = [5,6]
+c = np.c_[a,b,m]
+print(c)
